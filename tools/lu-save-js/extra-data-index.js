cd5b0197e2af955a47a75fdd4d2588d98425dd14
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2025-03-08 00:32:48 -0500

    lu-save-js: Further improvements to extra-data error handling
    A failure to retrieve one extra-data file will no longer prevent us from using the rest of them.

--- tools/lu-save-js/extra-data-index.js
+++ tools/lu-save-js/extra-data-index.js
@@ -29,19 +29,44 @@ let ExtraDataIndexManager = new (class ExtraDataIndexManager {
          for(let name of filenames) {
             promises[name] = fetch(`${base_path}${name}`);
          }
-         try {
-            await Promise.all(Object.values(promises));
-         } catch (e) {
-            // network error or we are running on file:///
-            pi.reject(e);
-            return coll;
+         {
+            let list = await Promise.allSettled(Object.values(promises));
+            
+            let any_succeeded = false;
+            let errors = [];
+            for(let info of list) {
+               if (info.status == "fulfilled") {
+                  any_succeeded = true;
+                  continue;
+               }
+               errors.push(info.reason);
+            }
+            if (errors.length) {
+               let err = new AggregateError(errors,
+                  any_succeeded ?
+                     "Some extra-data files for serialization version " + v + " could not be accessed."
+                  :
+                     "None of the extra-data for serialization version " + v + " files could be accessed."
+               );
+               if (!any_succeeded) {
+                  pi.reject(err);
+                  return;
+               }
+               console.warn(err);
+            }
          }
          
          let errors = [];
          let files  = new Map();
          let any    = false;
          for(let name of filenames) {
-            let blob = await (await promises[name]).arrayBuffer();
+            let response;
+            try {
+               response = await promises[name];
+            } catch (e) {
+               continue; // already handled above
+            }
+            let blob = await response.arrayBuffer();
             let file;
             try {
                file = new ExtraDataFile(blob);
@@ -56,12 +81,12 @@ let ExtraDataIndexManager = new (class ExtraDataIndexManager {
          coll.finalize(files);
          if (any) {
             if (errors.length) {
-               let err = new AggregateError(errors, "Some extra-data files in a collection failed to load.");
+               let err = new AggregateError(errors, "Some extra-data files for serialization version " + v + " failed to parse.");
                console.warn(err);
             }
             pi.resolve(coll);
          } else {
-            let err = new AggregateError(errors, "All extra-data files in a collection failed to load.");
+            let err = new AggregateError(errors, "All extra-data filess for serialization version " + v + " failed to parse.");
             pi.reject(err);
          }
       })();
